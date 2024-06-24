c8eb9031a366f66c11774c4c4fcdedc4088fcf56
manhtien-30 <tien310001@gmail.com>
2024-06-24 16:39:10 +0700

    init docker compose

---
+++ src/main/java/com/example/ecommerceapiwebsite/EcommerceApiWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceapiwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiWebsiteApplication.class, args);
+    }
+
+}
